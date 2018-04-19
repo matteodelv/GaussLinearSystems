aa30138082f21e8832bdf3a9a012570b5a5f6993
Matteo Del Vecchio <delvecchiomatteo96@gmail.com>
2018-04-19 09:38:32 +0200

    Iniziato il package.m e aggiunto caricamento dello stesso nel notebook

---
+++ GaussLinearSystemsPackage.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+BeginPackage["GaussLinearSystemsPackage`"];
+Unprotect["GaussLinearSystemsPackage`*"];
+ClearAll["GaussLinearSystemsPackage`*"];
+Begin["`Private`"];
+PrintHelloWorld[]=Module[{},Print["Hello World!"]];
+
+End[];
+
+
+EndPackage[];
